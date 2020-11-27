cea539b70b34d0d33eafebbae82f0cbf73b5f792
Solyannik Olga <solyannikolga@gmail.com>
2020-11-27 15:44:57 +0200

    changes in user.rb and question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
+<<<<<<< HEAD
 
+=======
+  validates :text, :user, presence: true
+>>>>>>> 672dc05... version 2
   validates :text, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ require 'openssl'
 class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
+<<<<<<< HEAD
   VALID_MAIL_REGEXP = URI::MailTo::EMAIL_REGEXP
   VALID_USERNAME_REGEXP = /\A\w+\z/
+=======
+  MAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  USERNAME = /\A[\w]+\z/
+>>>>>>> 672dc05... version 2
 
   has_many :questions
   before_validation :username_downcase
@@ -21,7 +26,11 @@ class User < ApplicationRecord
             length: { maximum: 40 },
             format: { with: VALID_USERNAME_REGEXP }
   validates :password, presence: true, on: :create
+<<<<<<< HEAD
   validates_confirmation_of :password
+=======
+  validates :password
+>>>>>>> 672dc05... version 2
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -35,9 +44,14 @@ class User < ApplicationRecord
       nil
     end
   end
+<<<<<<< HEAD
 
   private
+=======
+>>>>>>> 672dc05... version 2
 
+  private
+  
   def encrypt_password
     if self.password.present?
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
