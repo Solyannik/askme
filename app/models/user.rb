458f6d96b7605a897c64fba277a5d74c80447d3e
Solyannik Olga <solyannikolga@gmail.com>
2020-11-27 15:44:57 +0200

    add styles css

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
   MAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  USERNAME = /\A[a-zA-Z\d\_]+\z/
+  USERNAME = /\A[\w]+\z/
 
   has_many :questions
   before_validation :username_downcase
   before_validation :email_downcase
   before_save :encrypt_password
+
+  attr_accessor :password
 
   validates :email, presence: true,
                     uniqueness: true,
@@ -19,9 +21,23 @@ class User < ApplicationRecord
             length: { maximum: 40 },
             format: { with: USERNAME }
   validates :password, presence: true, on: :create
+  validates :password
 
-  attr_accessor :password
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
 
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+
+  private
+  
   def encrypt_password
     if self.password.present?
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
@@ -40,19 +56,6 @@ class User < ApplicationRecord
   def email_downcase
     if self.email.present?
       email.downcase!
-    end
-  end
-
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
-
-  def self.authenticate(email, password)
-    user = find_by(email: email)
-    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
-      user
-    else
-      nil
     end
   end
 end
