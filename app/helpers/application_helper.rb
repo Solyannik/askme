199fff313b7c23144f77bc95e728fdabd2960ce9
S.olya <solyannikolga@gmail.com>
2020-12-06 01:32:19 +0200

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,15 +8,6 @@ module ApplicationHelper
   end
 
   def inclination(number, one, few, many)
-<<<<<<< HEAD
-    return "" if number.nil? || number == 0
-    remainder_100 = number % 100
-    return many if remainder_100.between?(11, 14)
-    remainder = number % 10
-    return one if remainder == 1
-    return few if remainder.between?(2, 4)
-    return many
-=======
     return "0 #{many}" if (number.nil? || number == 0)
     remainder_100 = number % 100
     return many if remainder_100.between?(11, 14)
@@ -24,7 +15,6 @@ module ApplicationHelper
     return "#{number} #{one}" if remainder == 1
     return "#{number} #{few}" if remainder.between?(2, 4)
     "#{number} #{many}"
->>>>>>> 4b743a11dbedd6818b0cd5518806e42a25c7ec87
   end
 
   def fa_icon(icon_class)
