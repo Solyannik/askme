dcfbaaa4586acdb07f857b2d928b216bbcae131f
Solyannik Olga <solyannikolga@gmail.com>
2020-12-08 12:52:24 +0200

    change in user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
     else
       render 'new'
