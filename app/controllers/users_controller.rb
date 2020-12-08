99597ea42bdf17e20e383feace5a8ccaba4cd71f
Solyannik Olga <solyannikolga@gmail.com>
2020-12-08 12:37:59 +0200

    changes in button.css, new.html.erb, edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
   before_action :load_user, except: [:index, :create, :new]
-  before_action :authorize_user, except: [:index,  :new, :create, :show]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
     @users = User.all
