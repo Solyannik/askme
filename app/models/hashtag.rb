a3a62e13ec1a4099e13d7e8ab95c67943729b725
Solyannik Olga <solyannikolga@gmail.com>
2020-12-22 14:12:18 +0200

    Autocreate hashtags when question saves

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,6 +1,8 @@
 class Hashtag < ApplicationRecord
-has_many :hashtag_questions, dependent: :destroy
-has_many :questions, through: :hashtag_questions
+  REGEXP = /#[[:word:]-]+/
+
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :questions, through: :hashtag_questions
 
   validates :text, presence: true, uniqueness: true
 end
