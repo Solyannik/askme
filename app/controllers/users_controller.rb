361cc8e092e8792e63479179ee7660564a8eb92f
Solyannik Olga <solyannikolga@gmail.com>
2020-12-23 22:35:13 +0200

    change index.html.erb end users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.sorted
-    @hashtags = Hashtag.all.distinct.order(:name)
+    @hashtags = Hashtag.order(:text)
   end
 
   def new
