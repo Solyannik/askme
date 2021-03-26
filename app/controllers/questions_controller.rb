0481269cd87d42fd696ac977e58b8974c8ce55d1
Solyannik <solyannikolga@gmail.com>
2021-03-27 00:47:59 +0200

    add gem recaptcha

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.author_id = current_user.id if current_user.present?
 
-    if @question.save
+    if check_captcha(@question) && @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
       render :edit
@@ -46,5 +46,11 @@ class QuestionsController < ApplicationController
     else
       params.require(:question).permit(:user_id, :text)
     end
+  end
+
+  private
+
+  def check_captcha(model)
+    current_user.present? || verify_recaptcha(model: model)
   end
 end
